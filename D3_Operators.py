112a0559e676f432cb9334b9f4a23de787654115
Chandrakant Hatti <chandusayhi@users.noreply.github.com>
2022-05-26 16:04:23 +0530

    Create D3_Operators.py

---
+++ D3_Operators.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 26 15:44:21 2022
+
+@author: chandrakant
+"""
+
+#Arithmetic Operators in Python
+#Integers
+
+print('Addition: ', 1 + 2)
+print('Subtraction: ', 2 - 1)
+print("Multiplication: ", 2 * 3)
+print('Division: ', 4 / 2)
+print('Division: ', 6 / 2)
+print('Division: ', 7 / 2)
+print('Division without remainder: ', 7 // 2)
+print('Modulus: ', 3 % 2)
+print('Exponential: ', 3 ** 2)
+
+#Floating Numbers
+print('Floating number, PI: ', 3.14)
+print('Float Number, Gravity: ', 9.81)
+
+#Complex Numbers
+print('Complex NUmbers: ', 1 + 1j)
+print('Multiplying Complex Numbers: ', (1 + 1j) * (1 - 1j))
+
+#Arithmetic Operations and assigning the result to the variable
+a = 3  # a is a variable name and 3 is an integer data type
+b = 2  # b is a variable name and 2 is an integer data type
+
+total = a + b  
+diff = a - b
+product = a * b
+division = a / b
+remainder = a % b
+floor_division = a // b
+exponential = a ** b
+
+print(total)
+print('a + b = ', total)
+print('a - b = ', diff)
+print('a * b = ', product)
+print('a / b = ', division)
+print('a % b = ', remainder)
+print('a // b = ', floor_division)
+print(' a ** b =', exponential)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
